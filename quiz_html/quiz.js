57208be5b2c18d58590c4e52d7986b3a1222912c
Elina Axelrod <elina.akselrod@arttelit.com>
2025-10-17 10:30:06 +0300

    Add two question

--- quiz_html/quiz.js
+++ quiz_html/quiz.js
@@ -11,17 +11,25 @@ document.getElementById('checkAnswer3').addEventListener('click', function(){
   checkAnswer('book3')
 });
 
+document.getElementById('checkAnswer4').addEventListener('click', function(){
+  checkAnswer('song1')
+});
+
+document.getElementById('checkAnswer5').addEventListener('click', function(){
+  checkAnswer('picture1')
+});
+
 function checkAnswer(questionName) {
   const selectedBook = document.querySelector(`input[name="${questionName}"]:checked`);
   
   if (!selectedBook) {
-    alert('Пожалуйста, выберите книгу!');
+    alert('Пожалуйста, выберите правильный вариант!');
     return;
   }
   
   // Проверка правильности ответа для каждого вопроса
   if ((questionName === 'book1' && selectedBook.value === 'bednye_lyudi') ||
-      (questionName === 'book2' && selectedBook.value === 'sevastopolskie rasskazy') || (questionName === 'book3' && selectedBook.value === 'beliy pudel') ) {
+      (questionName === 'book2' && selectedBook.value === 'sevastopolskie rasskazy') || (questionName === 'book3' && selectedBook.value === 'beliy pudel') ||(questionName === 'song1' && selectedBook.value === 'mozart1') || (questionName === 'picture1' && selectedBook.value === 'alexandra_vladimirovna_vasnezova') ) {
     showModal('correctModal');
   } else {
     showModal('wrongModal');
